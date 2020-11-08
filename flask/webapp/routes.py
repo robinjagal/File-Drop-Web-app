51a0d6262e926ccdaf8c9da3f9dfb795d612acbd
robin <robinjagal31@gmail.com>
2020-11-08 13:57:09 +0530

    RTCpeerconnection and Datachannel is being established between two peers using WebRTC but the SDP needs to be pasted manually

--- flask/webapp/routes.py
+++ flask/webapp/routes.py
@@ -4,3 +4,11 @@ from flask import Flask,render_template
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/makeroom')
+def makeRoom():
+    return render_template('makeRoom.html')
+
+@app.route('/joinroom')
+def joinRoom():
+    return render_template('joinRoom.html')
